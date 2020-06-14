3198c4339d82f5279d88afe2235f7949303318bf
Nathan Chung <chung.nathanh@gmail.com>
2020-06-14 02:45:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-["wow", "I", "am", "really", "learning"]
+array = ["wow", "I", "am", "really", "learning"]
 array.shift["arrays!"]
 end
 
