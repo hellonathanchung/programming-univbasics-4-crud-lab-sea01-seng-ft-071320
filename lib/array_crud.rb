1634b0c1893f1fa1d1ec64e7da7a02fe9614ec9d
Nathan Chung <chung.nathanh@gmail.com>
2020-06-14 02:50:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,10 @@ end
 
 def add_element_to_start_of_array(array, element)
   array.unshift("wow")
-  
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
